835cb50b7f58e0b3e2fd553dfe06ce106e0f2362
Akshay Khole <akshay2009ark@gmail.com>
2012-01-24 20:54:24 +0530

    Created image button with acceptable attributes

--- client/AndroSync/gen/in/reversehack/androsync/R.java
+++ client/AndroSync/gen/in/reversehack/androsync/R.java
@@ -11,13 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int address_book=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon_contacts=0x7f020002;
+        public static final int plain_black_ori=0x7f020003;
+    }
+    public static final class id {
+        public static final int buttonSyncAll=0x7f050000;
+        public static final int imageButtonSyncContacts=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int screen_start=0x7f030000;
     }
     public static final class string {
+        public static final int ButtonSyncAll=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int stringImageContentDesc=0x7f040005;
+        public static final int stringOnClickNotifier=0x7f040004;
+        public static final int stringTitle=0x7f040002;
     }
 }
--- client/AndroSync/src/in/reversehack/androsync/AndroSyncMain.java
+++ client/AndroSync/src/in/reversehack/androsync/AndroSyncMain.java
@@ -9,5 +9,7 @@ public class AndroSyncMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_start);
+        
     }
+
 }
