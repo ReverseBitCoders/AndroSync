31073d1443eb77a2d66d07b10ea3a7930ac3b5d6
Akshay Khole <akshay2009ark@gmail.com>
2012-03-11 11:30:36 +0530

    Display drafts, sentbox, Another layout 4 smsSync

---
+++ client/AndroSync/src/in/reversehack/androsync/SmsSyncInboxActivity.java
@@ -0,0 +1,41 @@
+package in.reversehack.androsync;
+
+import android.app.ListActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class SmsSyncInboxActivity extends ListActivity {
+	
+	private ListAdapter adapter;
+	private static final Uri SMS_INBOX_URI = Uri.parse("content://sms/inbox");
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		displayInboxSms();
+
+	}
+
+	public void displayInboxSms() {
+		ContentResolver sms_cr = getContentResolver();
+		Cursor sms_cursor = sms_cr.query(SMS_INBOX_URI, null, null, null, null);
+		startManagingCursor(sms_cursor);
+
+		String[] columns = new String[] { "body" };
+		Log.d("sms",columns[0]);
+		int[] names = new int[] { R.id.inbox_rows };
+		
+		adapter = new SimpleCursorAdapter(this, R.layout.sms_list_view,
+				sms_cursor, columns, names);
+		setListAdapter(adapter);
+	}
+	
+};
